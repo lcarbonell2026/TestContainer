fcd44c580c01c56ebdaef3dffd312dc8aa2532d2
Luis Edgar Carbonell Cristys <lcarbonell@olimpiait.com>
2026-02-11 16:43:45 -0500

    add HolaControlesAdd HolaController to expose GET /hola

---
+++ TestContainer/Controllers/HolaController.cs
@@ -0,0 +1,12 @@
+﻿// File: Controllers/HolaController.cs
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("/")] // ← raíz del sitio
+public class HolaController : ControllerBase
+{
+    // GET /hola
+    [HttpGet("hola")]
+    public IActionResult Hola() => Ok("Hola desde staging!");
+}
+
